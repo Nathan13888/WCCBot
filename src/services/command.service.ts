0c3a496080a7054226a38d8038d1513beefe5e70
Justin Chung <20733699+justin13888@users.noreply.github.com>
2020-05-05 17:33:42 -0400

    Fixed linting issues

--- src/services/command.service.ts
+++ src/services/command.service.ts
@@ -1,20 +1,21 @@
+// eslint-disable-next-line no-unused-vars
 import * as Discord from 'discord.js';
-import { Logger } from '../utils/logger';
+import {Logger} from '../utils/logger';
 export class CommandService {
     parseCommand(msg: Discord.Message) {
         const cmd = msg.content.substring(2).toLowerCase();
         const args = cmd.split(' ');
 
         switch (args[0]) {
-            case 'ping':
-                msg.reply("pong");
-            case 'announce':
-                Logger.log('Announcing...');
-                // TODO: FINISH
-                break;
+        case 'ping':
+            msg.reply('pong');
+        case 'announce':
+            Logger.log('Announcing...');
+            // TODO: FINISH
+            break;
 
-            default:
-                msg.author.send('Invalid command. Please specify a command.');
+        default:
+            msg.author.send('Invalid command. Please specify a command.');
         }
     }
 }
