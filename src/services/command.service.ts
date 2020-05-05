92a353356cb55c0e897cecde5eeb0ce78b425531
Justin Chung <20733699+justin13888@users.noreply.github.com>
2020-05-05 17:39:49 -0400

    Added logging function and fixed some bugs

--- src/services/command.service.ts
+++ src/services/command.service.ts
@@ -3,19 +3,19 @@ import * as Discord from 'discord.js';
 import {Logger} from '../utils/logger';
 export class CommandService {
     parseCommand(msg: Discord.Message) {
+        Logger.log(`${msg.author.username}#${msg.author.discriminator} (COMMAND): ${msg.content}`);
         const cmd = msg.content.substring(2).toLowerCase();
         const args = cmd.split(' ');
-
         switch (args[0]) {
         case 'ping':
             msg.reply('pong');
+            break;
         case 'announce':
-            Logger.log('Announcing...');
             // TODO: FINISH
             break;
 
         default:
-            msg.author.send('Invalid command. Please specify a command.');
+            msg.reply('Invalid command. Please specify a command.');
         }
     }
 }
