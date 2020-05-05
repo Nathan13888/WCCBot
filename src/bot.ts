e8c7035da6e584c20e5a3cd26c0c16c8f81ba445
Nathan <29968201+Nathan13888@users.noreply.github.com>
2020-05-05 16:01:50 -0400

    finish logger

--- src/bot.ts
+++ src/bot.ts
@@ -2,32 +2,32 @@ import * as Discord from "discord.js";
 import { CommandService } from "./services/command.service";
 import { Logger } from "./utils/logger";
 
+let api: Discord.Client;
 export class Bot {
-  api: Discord.Client;
   embed: Discord.MessageEmbed;
 
   discordToken: string;
   commandService: CommandService;
 
   constructor() {
-    this.api = new Discord.Client();
+    api = new Discord.Client();
     if (process.env.DISCORD_TOKEN)
       this.discordToken = process.env.DISCORD_TOKEN;
     else throw new Error("Discord token needed."); // TODO: Hook to Logger
     this.commandService = new CommandService();
 
     this.init();
-    this.api.login(this.discordToken);
+    api.login(this.discordToken);
   }
 
   init() {
-    this.api.once("ready", () => {
+    api.once("ready", () => {
       Logger.log("WCC Bot has started!");
       Logger.log(`Connected as ${this.api.user.tag}`);
       this.api.user.setActivity("Chess and w!", { type: "PLAYING" });
     });
 
-    this.api.on("message", (msg) => {
+    api.on("message", (msg) => {
       if (msg.author.bot) return;
       if (msg.content.substring(0, 2) == "w!")
         this.commandService.parseCommand(msg);
--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,6 +1,6 @@
 export namespace Logger {
     export function log(msg: string) {
         console.log(msg);
-        // TODO: Log to #bot-log
+        this.api.channels.get(process.env.LOG).send();
     }
 }
