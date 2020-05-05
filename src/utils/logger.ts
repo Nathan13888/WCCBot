6dd438323bb2a21ee30fa0b9927a7c23b273c64b
Nathan <29968201+Nathan13888@users.noreply.github.com>
2020-05-05 19:04:17 -0400

    fixed Logger to log to #bot-log

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,10 +1,12 @@
+import { TextChannel } from "discord.js";
+import { Bot } from "../bot";
 
 export namespace Logger {
     export async function log(msg: string) {
         console.log(msg);
         // TODO: fix this, its broken for whatever reason
-        // let channel = Bot.api.channels.cache.get(process.env.LOG);
-        // if(channel.type == "text")
-            // channel.send(msg);
+        let channel = Bot.api.channels.cache.get(process.env.LOG);
+        if(channel.type == "text")
+            (channel as TextChannel).send(msg);
     }
 }
