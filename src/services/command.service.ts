3b8e87edbabd9f3ca3e3b295914d0f704346da65
Nathan <29968201+Nathan13888@users.noreply.github.com>
2020-05-05 22:22:53 -0400

    add permission stuff to Cammands

--- src/services/command.service.ts
+++ src/services/command.service.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import * as Discord from 'discord.js';
-import {Logger} from '../utils/logger';
-import {Bot} from '../bot';
+import { Bot } from '../bot';
+import { Logger } from '../utils/logger';
 export namespace CommandService {
     export function registerCommands() {
         Bot.api.on('message', async (msg) => {
@@ -15,41 +15,48 @@ export namespace CommandService {
     export function parseCommand(msg: Discord.Message) {
         Logger.log(
             `${msg.author.tag} executed '${msg.content}'`);
-        const args = msg.content.slice(2).split(/\s+/);
-        const cmd = args.shift().toLowerCase();
-        console.log(cmd);
-        console.log(args);
-        // const cmd = msg.content.substring(2).toLowerCase();
-        // const args = cmd.split(/ +/);
+        // const args = msg.content.slice(2).split(/\s+/);
+        // const cmd = args.shift().toLowerCase();
+        // console.log(cmd);
+        // console.log(args);
+        const cmd = msg.content.substring(2).toLowerCase();
+        const args = cmd.split(/ +/);
 
-        switch (args[0]) { }
-
-        // TODO: check if user has permission
-        switch (cmd) {
-        case 'alive':
-            msg.react('👍');
-            break;
-        case 'shutdown':
-            Logger.log('Shutting down');
-            process.exit();
-            break;
-        case 'announce':
-            if (args.length!==2) {
-                msg.channel.send('Invalid number of arguments.');
+        switch (args[0]) {
+            case 'alive':
+                msg.react('👍');
                 break;
-            }
-            const title = 'Lorum Ipsum';
-            const message = 'lorum ipsum';
-            const embed = new Discord.MessageEmbed()
-                // .setAuthor()
-                .setColor(0xd62320)
-                .setTitle(title)
-                .setDescription(message)
-                .setFooter(msg.author.tag, msg.author.avatarURL());
-            msg.channel.send(embed);
-            break;
-        default:
-            msg.channel.send('Command not found');
+            case 'help':
+                // msg.react(':regional_indicator_w:');
+                // msg.react(':regional_indicator_i:');
+                // msg.react(':regional_indicator_p:');
+                msg.channel.send("W.I.P");
+                break;
+            default:
+                if (msg.member.roles.cache.some(role => role.name.toLowerCase().startsWith("bot"))/*||msg.channel.id*/)
+                    switch (cmd) {
+                        case 'shutdown':
+                            Logger.log('Shutting down');
+                            process.exit();
+                            break;
+                        case 'announce':
+                            if (args.length !== 2) {
+                                msg.channel.send('Invalid number of arguments.');
+                                break;
+                            }
+                            const title = 'Lorum Ipsum';
+                            const message = 'lorum ipsum';
+                            const embed = new Discord.MessageEmbed()
+                                // .setAuthor()
+                                .setColor(0xd62320)
+                                .setTitle(title)
+                                .setDescription(message)
+                                .setFooter(msg.author.tag, msg.author.avatarURL());
+                            msg.channel.send(embed);
+                            break;
+                        default:
+                            msg.channel.send('Command not found');
+                    }
         }
     }
 }
