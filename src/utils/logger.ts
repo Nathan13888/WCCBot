e8b5658faae9a5812b0e66f9479d37b1ad418175
Justin Chung <20733699+justin13888@users.noreply.github.com>
2020-05-05 20:25:33 -0400

    Linted

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,11 +1,13 @@
-import { TextChannel } from "discord.js";
-import { Bot } from "../bot";
+// eslint-disable-next-line no-unused-vars
+import {TextChannel} from 'discord.js';
+import {Bot} from '../bot';
 
 export namespace Logger {
     export async function log(msg: string) {
         console.log(msg);
-        let channel = Bot.api.channels.cache.get(process.env.LOG);
-        if(channel.type == "text")
+        const channel = Bot.api.channels.cache.get(process.env.LOG);
+        if (channel.type == 'text') {
             (channel as TextChannel).send(msg);
+        }
     }
 }
