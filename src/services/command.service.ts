fc58488da6c2a451139db7bd2451cd81677a265e
Nathan <29968201+Nathan13888@users.noreply.github.com>
2020-05-06 17:51:18 -0400

    organize commands

--- src/services/command.service.ts
+++ src/services/command.service.ts
@@ -1,7 +1,7 @@
 // eslint-disable-next-line no-unused-vars
 import * as Discord from 'discord.js';
-import {Bot} from '../bot';
-import {Logger} from '../utils/logger';
+import { Bot } from '../bot';
+import { Logger } from '../utils/logger';
 export namespace CommandService {
     export function registerCommands() {
         Bot.api.on('message', async (msg) => {
@@ -21,70 +21,70 @@ export namespace CommandService {
 
         // TODO: check if user has permission
         switch (cmd) {
-        case 'alive':
-            msg.react('👍');
-            break;
-        case 'help':
-            // msg.react(':regional_indicator_w:');
-            // msg.react(':regional_indicator_i:');
-            // msg.react(':regional_indicator_p:');
-            msg.channel.send('W.I.P');
-            break;
-        case 'shutdown':
-            Logger.log('Shutting down');
-            process.exit();
-            break;
-        case 'announce':
-            const title = await promptInput(
-                'Please tell me the title.',
-                msg.channel, msg.author);
-            if (!title) {
-                msg.channel.send('Timed out. Please try again.');
+            case 'alive':
+                msg.react('👍');
                 break;
-            }
-            const message = await promptInput(
-                'Please tell me the message.',
-                msg.channel, msg.author, 120000);
-            if (!message) {
-                msg.channel.send('Timed out. Please try again.');
+            case 'help':
+                // msg.react(':regional_indicator_w:');
+                // msg.react(':regional_indicator_i:');
+                // msg.react(':regional_indicator_p:');
+                msg.channel.send('W.I.P');
                 break;
-            }
-            msg.channel.send(new Discord.MessageEmbed()
-                // .setAuthor()
-                .setColor(0xd62320)
-                .setTitle(title)
-                .setDescription(message)
-                .setFooter(msg.author.tag, msg.author.avatarURL()),
-            );
-            break;
-        default:
-            if (msg.member.roles.cache.some(
-                (role) => role.name.toLowerCase().startsWith('bot'),
-            )/* ||msg.channel.id*/) {
-                switch (cmd) {
-                case 'shutdown':
-                    Logger.log('Shutting down');
-                    process.exit();
+            case 'shutdown':
+                Logger.log('Shutting down');
+                process.exit();
+                break;
+            case 'announce':
+                const title = await promptInput(
+                    'Please tell me the title.',
+                    msg.channel, msg.author);
+                if (!title) {
+                    msg.channel.send('Timed out. Please try again.');
                     break;
-                case 'announce':
-                    if (args.length !== 2) {
-                        msg.channel.send('Invalid number of arguments.');
-                        break;
-                    }
-                    const title = 'Lorum Ipsum';
-                    const message = 'lorum ipsum';
-                    const embed = new Discord.MessageEmbed()
-                    // .setAuthor()
-                        .setColor(0xd62320)
-                        .setTitle(title)
-                        .setDescription(message)
-                        .setFooter(msg.author.tag, msg.author.avatarURL());
-                    msg.channel.send(embed);
-                    break;
-                default:
-                    msg.channel.send('Command not found');
                 }
-            }
+                const message = await promptInput(
+                    'Please tell me the message.',
+                    msg.channel, msg.author, 120000);
+                if (!message) {
+                    msg.channel.send('Timed out. Please try again.');
+                    break;
+                }
+                msg.channel.send(new Discord.MessageEmbed()
+                    // .setAuthor()
+                    .setColor(0xd62320)
+                    .setTitle(title)
+                    .setDescription(message)
+                    .setFooter(msg.author.tag, msg.author.avatarURL()),
+                );
+                break;
+            default:
+                if (msg.member.roles.cache.some(
+                    (role) => role.name.toLowerCase().startsWith('bot'),
+                )/* ||msg.channel.id*/) {
+                    switch (cmd) {
+                        case 'shutdown':
+                            Logger.log('Shutting down');
+                            process.exit();
+                            break;
+                        case 'announce':
+                            if (args.length !== 2) {
+                                msg.channel.send('Invalid number of arguments.');
+                                break;
+                            }
+                            const title = 'Lorum Ipsum';
+                            const message = 'lorum ipsum';
+                            const embed = new Discord.MessageEmbed()
+                                // .setAuthor()
+                                .setColor(0xd62320)
+                                .setTitle(title)
+                                .setDescription(message)
+                                .setFooter(msg.author.tag, msg.author.avatarURL());
+                            msg.channel.send(embed);
+                            break;
+                        default:
+                            msg.channel.send('Command not found');
+                    }
+                }
         }
     }
 
@@ -96,8 +96,8 @@ export namespace CommandService {
     ) {
         let input: string | undefined;
         await channel.send(question).then(async () => {
-            if (!timeLimit) timeLimit=60000;
-            const filter = (msg) => user.id===msg.author.id;
+            if (!timeLimit) timeLimit = 60000;
+            const filter = (msg) => user.id === msg.author.id;
             await channel.awaitMessages(filter, {
                 time: timeLimit, max: 1, errors: ['time'],
             })
