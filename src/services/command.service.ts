e4fc5331c5e3df26a579b96880b432dd98a7a26d
Justin Chung <20733699+justin13888@users.noreply.github.com>
2020-05-06 17:26:09 -0400

    Fully implemented command permissions

--- src/services/command.service.ts
+++ src/services/command.service.ts
@@ -1,5 +1,5 @@
-// eslint-disable-next-line no-unused-vars
-import * as Discord from 'discord.js';
+import {TextChannel, DMChannel, NewsChannel,
+    Message, MessageEmbed, User} from 'discord.js';
 import {Bot} from '../bot';
 import {Logger} from '../utils/logger';
 export namespace CommandService {
@@ -12,7 +12,7 @@ export namespace CommandService {
         });
     }
 
-    export async function parseCommand(msg: Discord.Message) {
+    export async function parseCommand(msg: Message) {
         Logger.log(
             `${msg.author.tag} executed '${msg.content}'`);
         // TODO: Improve regex to support single and double quotes.
@@ -30,33 +30,6 @@ export namespace CommandService {
             // msg.react(':regional_indicator_p:');
             msg.channel.send('W.I.P');
             break;
-        case 'shutdown':
-            Logger.log('Shutting down');
-            process.exit();
-            break;
-        case 'announce':
-            const title = await promptInput(
-                'Please tell me the title.',
-                msg.channel, msg.author);
-            if (!title) {
-                msg.channel.send('Timed out. Please try again.');
-                break;
-            }
-            const message = await promptInput(
-                'Please tell me the message.',
-                msg.channel, msg.author, 120000);
-            if (!message) {
-                msg.channel.send('Timed out. Please try again.');
-                break;
-            }
-            msg.channel.send(new Discord.MessageEmbed()
-                // .setAuthor()
-                .setColor(0xd62320)
-                .setTitle(title)
-                .setDescription(message)
-                .setFooter(msg.author.tag, msg.author.avatarURL()),
-            );
-            break;
         default:
             if (msg.member.roles.cache.some(
                 (role) => role.name.toLowerCase().startsWith('bot'),
@@ -67,34 +40,55 @@ export namespace CommandService {
                     process.exit();
                     break;
                 case 'announce':
-                    if (args.length !== 2) {
-                        msg.channel.send('Invalid number of arguments.');
+                    const title = await promptInput(
+                        'Please tell me the title.',
+                        msg.channel, msg.author);
+                    if (!title) {
+                        msg.channel.send('Timed out. Please try again.');
                         break;
                     }
-                    const title = 'Lorum Ipsum';
-                    const message = 'lorum ipsum';
-                    const embed = new Discord.MessageEmbed()
+                    const message = await promptInput(
+                        'Please tell me the message.',
+                        msg.channel, msg.author, 120000);
+                    if (!message) {
+                        msg.channel.send('Timed out. Please try again.');
+                        break;
+                    }
+                    const embed = new MessageEmbed()
                     // .setAuthor()
                         .setColor(0xd62320)
                         .setTitle(title)
                         .setDescription(message)
                         .setFooter(msg.author.tag, msg.author.avatarURL());
+
+                    const confirmation = await promptInput(
+                        'Please confirm the announcement. (Yes/no)',
+                        msg.channel, msg.author, 30000);
+                    if (!(confirmation && confirmation.toLowerCase()==='yes')) {
+                        msg.channel.send(
+                            'Your announcement has been cancelled.',
+                        );
+                        break;
+                    }
+                    // TODO: Connect to announcement channel
                     msg.channel.send(embed);
                     break;
                 default:
                     msg.channel.send('Command not found');
                 }
+            } else {
+                msg.channel.send('Command not found');
             }
         }
     }
 
     async function promptInput(
-        question: string,
-        channel: Discord.TextChannel | Discord.DMChannel | Discord.NewsChannel,
-        user: Discord.User,
+        question: string, // TODO: Add more types
+        channel: TextChannel | DMChannel | NewsChannel,
+        user: User,
         timeLimit?: number,
     ) {
-        let input: string | undefined;
+        let input: string;
         await channel.send(question).then(async () => {
             if (!timeLimit) timeLimit=60000;
             const filter = (msg) => user.id===msg.author.id;
