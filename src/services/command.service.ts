c50bb397f5db90a23483103838052c49c48ef60a
Nathan13888 <29968201+Nathan13888@users.noreply.github.com>
2020-07-26 16:55:25 -0400

    command: randomopening

--- src/services/command.service.ts
+++ src/services/command.service.ts
@@ -14,26 +14,33 @@ export namespace CommandService {
     '(\\d{1,4}) +(0\\d|1[0-2]) +(0\\d|[12]\\d|3[01]) +',
     '([01]\\d|2[0-3]) +([0-5]\\d)( +[0-5]\\d)?',
   ].join(''));
-    export async function registerCommands() {
-      Bot.api.on('message', async (msg) => {
-        if (msg.author.bot) return;
-        if (msg.content.substring(0, 2) === '::') {
-          parseCommand(msg);
-        }
-      });
-    }
+  export async function registerCommands() {
+    Bot.api.on('message', async (msg) => {
+      if (msg.author.bot) return;
+      if (msg.content.substring(0, 2) === '::') {
+        parseCommand(msg);
+      }
+    });
+  }
 
-    // TODO: Use reply to make more clear replies to incorrect command usages
-    async function parseCommand(msg: Message) {
-      Logger.log(
-        `${msg.author.tag} executed \`${msg.content}\``);
-      // TODO: Improve regex to support single and double quotes.
-      const args = msg.content.slice(2).split(/ +/);
-      const cmd = args.shift().toLowerCase();
+  // TODO: Use reply to make more clear replies to incorrect command usages
+  async function parseCommand(msg: Message) {
+    Logger.log(
+      `${msg.author.tag} executed \`${msg.content}\``);
+    // TODO: Improve regex to support single and double quotes.
+    const args = msg.content.slice(2).split(/ +/);
+    const cmd = args.shift().toLowerCase();
 
-      switch (cmd) {
+    switch (cmd) {
       case 'alive':
         msg.react('👍');
+        break;
+      case 'randomopening':
+        var url = 'https://www.365chess.com/eco/';
+        const LETTERS = "ABCDE";
+        url += LETTERS.charAt(Math.floor(Math.random() * LETTERS.length));
+        url += Math.floor(Math.random() * 100) + 1;
+        msg.reply('Here\'s a random opening: \n' + url);
         break;
       case 'help':
         msg.react('👎');
@@ -50,17 +57,29 @@ export namespace CommandService {
             (role) => role.name.toLowerCase().startsWith('bot'),
           )/* ||msg.channel.id*/) {
             switch (cmd) {
-            case 'shutdown':
-              Logger.log('Shutting down');
-              process.exit();
+              case 'shutdown':
+                Logger.log('Shutting down');
+                process.exit();
               // break;
-            case 'remind':
-              if (args&&args.length) {
-                if (args.length>1) {
-                  msg.channel.send('Too many arguments!');
-                  return;
-                }
-                if (args[0].toLowerCase()==='now') {
+              case 'remind':
+                if (args && args.length) {
+                  if (args.length > 1) {
+                    msg.channel.send('Too many arguments!');
+                    return;
+                  }
+                  if (args[0].toLowerCase() === 'now') {
+                    const content = await promptInput(
+                      'What is the message of the reminder?',
+                      msg.channel, msg.author);
+                    if (!content) {
+                      msg.channel.send('Timed out. Please try again.');
+                      return;
+                    }
+                    ReminderService.sendReminder(content, msg.author);
+                  } else {
+                    msg.channel.send('Invalid time argument.');
+                  }
+                } else {
                   const content = await promptInput(
                     'What is the message of the reminder?',
                     msg.channel, msg.author);
@@ -68,108 +87,96 @@ export namespace CommandService {
                     msg.channel.send('Timed out. Please try again.');
                     return;
                   }
-                  ReminderService.sendReminder(content, msg.author);
-                } else {
-                  msg.channel.send('Invalid time argument.');
+                  const date = await promptInput(
+                    'Enter the date (YYYY MM DD hh mm ss)',
+                    msg.channel, msg.author);
+                  if (!date) {
+                    msg.channel.send('Timed out. Please try again.');
+                    return;
+                  }
+                  const match = dateRegex.exec(date);
+                  if (match) {
+                    const dateObj = new Date(
+                      parseInt(match[1]),
+                      parseInt(match[2]) - 1,
+                      parseInt(match[3]),
+                      parseInt(match[4]),
+                      parseInt(match[5]),
+                      match[6] ? parseInt(match[6]) : 0,
+                    );
+                    ReminderService.setReminder(dateObj, content, msg.author);
+                    msg.channel.send('Reminder is set.');
+                  } else {
+                    msg.channel.send('Invalid date format.');
+                    return;
+                  }
                 }
-              } else {
-                const content = await promptInput(
-                  'What is the message of the reminder?',
-                  msg.channel, msg.author);
-                if (!content) {
-                  msg.channel.send('Timed out. Please try again.');
-                  return;
-                }
-                const date = await promptInput(
-                  'Enter the date (YYYY MM DD hh mm ss)',
-                  msg.channel, msg.author);
-                if (!date) {
-                  msg.channel.send('Timed out. Please try again.');
-                  return;
-                }
-                const match = dateRegex.exec(date);
-                if (match) {
-                  const dateObj = new Date(
-                    parseInt(match[1]),
-                    parseInt(match[2])-1,
-                    parseInt(match[3]),
-                    parseInt(match[4]),
-                    parseInt(match[5]),
-                    match[6]?parseInt(match[6]):0,
-                  );
-                  ReminderService.setReminder(dateObj, content, msg.author);
-                  msg.channel.send('Reminder is set.');
-                } else {
-                  msg.channel.send('Invalid date format.');
-                  return;
-                }
-              }
-              break;
-            case 'announce':
-              const title = await promptInput(
-                'Please tell me the title.',
-                msg.channel, msg.author);
-              if (!title) {
-                msg.channel.send('Timed out. Please try again.');
-                return;
-              }
-              const message = await promptInput(
-                'Please tell me the message.',
-                msg.channel, msg.author, 120000);
-              if (!message) {
-                msg.channel.send('Timed out. Please try again.');
-                return;
-              }
-              const embed = new MessageEmbed()
-                // .setAuthor()
-                .setColor(0xd62320)
-                .setTitle(title)
-                .setDescription(message)
-                .setFooter(msg.author.tag, msg.author.avatarURL());
-
-              const confirmation = await promptInput(
-                'Please confirm the announcement. (Yes/no)',
-                msg.channel, msg.author, 30000);
-              if (!(confirmation && confirmation.toLowerCase()==='yes')) {
-                msg.channel.send(
-                  'Your announcement has been cancelled.',
-                );
                 break;
-              }
-              Bot.announcementChannel.send(embed);
+              case 'announce':
+                const title = await promptInput(
+                  'Please tell me the title.',
+                  msg.channel, msg.author);
+                if (!title) {
+                  msg.channel.send('Timed out. Please try again.');
+                  return;
+                }
+                const message = await promptInput(
+                  'Please tell me the message.',
+                  msg.channel, msg.author, 120000);
+                if (!message) {
+                  msg.channel.send('Timed out. Please try again.');
+                  return;
+                }
+                const embed = new MessageEmbed()
+                  // .setAuthor()
+                  .setColor(0xd62320)
+                  .setTitle(title)
+                  .setDescription(message)
+                  .setFooter(msg.author.tag, msg.author.avatarURL());
+
+                const confirmation = await promptInput(
+                  'Please confirm the announcement. (Yes/no)',
+                  msg.channel, msg.author, 30000);
+                if (!(confirmation && confirmation.toLowerCase() === 'yes')) {
+                  msg.channel.send(
+                    'Your announcement has been cancelled.',
+                  );
+                  break;
+                }
+                Bot.announcementChannel.send(embed);
             }
             break;
           }
         }
         msg.channel.send('Command not found');
-      }
     }
+  }
 
-    async function promptInput(
-      question: StringResolvable | MessageOptions
-            | (MessageOptions & { split?: false })
-            | MessageAdditions | APIMessage,
-      channel: TextChannel | DMChannel | NewsChannel,
-      user: User,
-      timeLimit?: number,
-    ) {
-      let input: string;
-      await channel.send(question).then(async () => {
-        if (!timeLimit) timeLimit = 60000;
-        const filter = (msg) => user.id === msg.author.id;
-        await channel.awaitMessages(filter, {
-          time: timeLimit, max: 1, errors: ['time'],
+  async function promptInput(
+    question: StringResolvable | MessageOptions
+      | (MessageOptions & { split?: false })
+      | MessageAdditions | APIMessage,
+    channel: TextChannel | DMChannel | NewsChannel,
+    user: User,
+    timeLimit?: number,
+  ) {
+    let input: string;
+    await channel.send(question).then(async () => {
+      if (!timeLimit) timeLimit = 60000;
+      const filter = (msg) => user.id === msg.author.id;
+      await channel.awaitMessages(filter, {
+        time: timeLimit, max: 1, errors: ['time'],
+      })
+        .then((messages) => {
+          input = messages.first().content;
+          channel.send(
+            `You've entered: ${input}`,
+          );
         })
-          .then((messages) => {
-            input = messages.first().content;
-            channel.send(
-              `You've entered: ${input}`,
-            );
-          })
-          .catch(() => {
-            channel.send('You did not enter any input!');
-          });
-      });
-      return input;
-    }
+        .catch(() => {
+          channel.send('You did not enter any input!');
+        });
+    });
+    return input;
+  }
 }
